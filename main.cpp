b673fb77bf82746cf25472d4189fb43c60366973
lixc <xichao@sina.cn>
2025-06-23 12:49:47 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,100 @@
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>  // cv::Canny()
+#include <iostream>
+
+using namespace cv;
+using std::cout; using std::cerr; using std::endl;
+
+int main(int, char**)
+{
+    Mat frame;
+    cout << "Opening camera..." << endl;
+    VideoCapture capture(0, CAP_V4L2); // open the first camera
+    if (!capture.isOpened())
+    {
+        cerr << "ERROR: Can't initialize camera capture" << endl;
+        return 1;
+    }
+
+    bool fourcc_set = capture.set(CAP_PROP_FOURCC, VideoWriter::fourcc('M', 'J', 'P', 'G'));
+#ifndef NDEBUG
+    printf("fourcc set: %d\n", fourcc_set);
+#endif
+
+    capture.set(CAP_PROP_FRAME_WIDTH, 1920);
+    capture.set(CAP_PROP_FRAME_HEIGHT, 1080);
+    capture.set(CAP_PROP_FPS, 30);
+
+    int frame_width = capture.get(CAP_PROP_FRAME_WIDTH);
+    int frame_height = capture.get(CAP_PROP_FRAME_HEIGHT);
+
+    cout << "Frame width: " << frame_width << endl;
+    cout << "     height: " << frame_height << endl;
+    cout << "Capturing FPS: " << capture.get(CAP_PROP_FPS) << endl;
+
+    cout << endl << "Press 'ESC' to quit, 'space' to toggle frame processing" << endl;
+    cout << endl << "Start grabbing..." << endl;
+
+    size_t nFrames = 0;
+    bool enableProcessing = false;
+    int64 t0 = cv::getTickCount();
+    int64 processingTime = 0;
+
+    double avg_fps = 0.0;
+    char fps_text[32];
+    char res_text[32];
+
+    for (;;)
+    {
+        capture >> frame; // read the next frame from camera
+        if (frame.empty())
+        {
+            cerr << "ERROR: Can't grab camera frame." << endl;
+            break;
+        }
+        nFrames++;
+        if (nFrames % 10 == 0)
+        {
+            const int N = 10;
+            int64 t1 = cv::getTickCount();
+            avg_fps = static_cast<double>(getTickFrequency()) * N / (static_cast<double>(t1 - t0));
+            cout << "Frames captured: " << cv::format("%5lld", (long long int)nFrames)
+                 << "    Average FPS: " << cv::format("%9.1f", avg_fps)
+                 << "    Average time per frame: " << cv::format("%9.2f ms", static_cast<double>(t1 - t0) * 1000.0f / (N * getTickFrequency()))
+                 << "    Average processing time: " << cv::format("%9.2f ms", static_cast<double>(processingTime) * 1000.0f / (N * getTickFrequency()))
+                 << std::endl;
+            t0 = t1;
+            processingTime = 0;
+        }
+        if (!enableProcessing)
+        {
+            sprintf(fps_text, "FPS: %.2f", avg_fps);
+            sprintf(res_text,"Resolution:%d*%d",frame_width,frame_height);
+            cv::putText(frame, fps_text, cv::Point(10, 30),
+                     cv::FONT_HERSHEY_SIMPLEX, 1.0, cv::Scalar(0, 255, 0), 2);
+            cv::putText(frame, res_text, cv::Point(10, 70),
+                     cv::FONT_HERSHEY_SIMPLEX, 1.0, cv::Scalar(255, 255, 0), 2);
+            imshow("Frame", frame);
+        }
+        else
+        {
+            int64 tp0 = cv::getTickCount();
+            Mat processed;
+            cv::Canny(frame, processed, 400, 1000, 5);
+            processingTime += cv::getTickCount() - tp0;
+            imshow("Frame", processed);
+        }
+        int key = waitKey(1);
+        if (key == 27/*ESC*/)
+            break;
+        if (key == 32/*SPACE*/)
+        {
+            enableProcessing = !enableProcessing;
+            cout << "Enable frame processing ('space' key): " << enableProcessing << endl;
+        }
+    }
+    std::cout << "Number of captured frames: " << nFrames << endl;
+    return nFrames > 0 ? 0 : 1;
+}
